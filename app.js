dc31a6c6fcd82ec7e4b1a8a6f04e81fa3615db5f
prashanth-420 <ramprashanth131@gmail.com>
2025-10-30 18:50:28 +0530

    Trigger GitHub contributor cache refresh

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('./AppServer/models/db');
 var indexRouter = require('./AppServer/routes/index');
 var usersRouter = require('./AppServer/routes/users');
 
